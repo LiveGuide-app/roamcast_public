9c9012a677ac757570a1bf0fb0ce5555d5047a03
Remo Rohner <your.email@example.com>
2025-03-31 16:55:05 +0100

    Add configuration files

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+console.log('Loading babel.config.js');
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: ['EXPO_PUBLIC_SUPABASE_URL', 'EXPO_PUBLIC_SUPABASE_ANON_KEY', 'EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY', 'EXPO_PUBLIC_APP_URL_SCHEME'],
+        safe: false,
+        allowUndefined: true,
+      }],
+    ],
+  };
+}; 
